371fcf3430db5c2d3d723e057c62836fa0f7bd14
Lisa Trovato-Monastra <l.trovato-monastra@posteo.de>
2023-07-27 18:05:47 +0200

    added missing homepage in package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "./styles/App.css";
 
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <div className="App">
+      <h1>hi</h1>
+    </div>
+  );
 }
 
 export default App;
